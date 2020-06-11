c9b78da212bfe5576aa8bb718b1e45c9bba165e1
김현준 <first0506@naver.com>
2020-06-11 18:07:55 +0900

    모델 구성완료, 데이터 입력완료, 디테일 페이지 진행중

--- final_pjt/movies/models.py
+++ final_pjt/movies/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    original_title = models.CharField(max_length=100)
+    release_date = models.DateField()
+    popularity = models.FloatField()
+    vote_count = models.IntegerField()
+    vote_average = models.FloatField()
+    adult = models.BooleanField()
+    overview = models.TextField()
+    original_language = models.CharField(max_length=100)
+    poster_path = models.CharField(max_length=100)
+    backdrop_path = models.CharField(max_length=100)
+    genres = models.ManyToManyField(Genre, related_name='genre_movie')
+    rank = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='rank_movies')
+
+class Review(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_articles')
+    hate_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='hate_articles')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
